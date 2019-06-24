596caad80d81557a3a1d4cfe248fff04bb244e34
Katrina R <katrina.reinsdorff@gmail.com>
2019-06-24 20:07:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return string.toUpperCase()
+} 
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('hello'.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log('hello'.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var lowercase = 'hello'
+  if ('hello'.toLowerCase() === lowercase)
+  return "I can\'t hear you!"
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO"
+  if ("HELLO".toUpperCase() === uppercase)
+  return "YES INDEED!"
+}
+
+  
